8adff6907b1610d4b48580a2627ac3094731f861
bnb <bnbtrikala@gmail.com>
2024-02-17 20:30:22 +0200

    Finish calculator layout

---
+++ code.js
@@ -0,0 +1,28 @@
+let number1;
+let number2;
+let operant;
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, num2, operant) {
+  
+}
+
+console.log(add(15, 11));
+console.log(subtract(15, 11));
+console.log(multiply(15, 11));
+console.log(divide(15, 10));
