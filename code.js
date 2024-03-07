df27d40c0d7db39f3e602385c68a12f856214896
bnb <bnbtrikala@gmail.com>
2024-03-07 17:18:45 +0200

    Finish clear and delete buttons

--- code.js
+++ code.js
@@ -2,6 +2,36 @@ let number1;
 let number2;
 let operant;
 
+const clearBtn = document.querySelector('.clear');
+const deleteBtn = document.querySelector('.delete');
+const screen = document.querySelector('.screen');
+let buttons = document.querySelectorAll('.calcBtns');
+
+
+buttons.forEach((button) => {
+    button.textContent = button.dataset.name;
+});
+
+
+clearBtn.addEventListener('click', () => {
+  console.log('clear');
+  clear();
+})
+
+deleteBtn.addEventListener('click', () => {
+  console.log('delete');
+  screen.textContent =  deleted();
+})
+
+function deleted() {
+  let screenText = screen.textContent;
+  return screenText.slice(0,-1);
+}
+
+function clear() {
+  screen.textContent = '';
+}
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -22,7 +52,17 @@ function operate(num1, num2, operant) {
   
 }
 
+buttons.forEach(function(button) {
+  button.addEventListener('click',() =>{
+    console.log(button.dataset.name);
+    screen.textContent += button.dataset.name; 
+  })
+})
+
+
 console.log(add(15, 11));
 console.log(subtract(15, 11));
 console.log(multiply(15, 11));
 console.log(divide(15, 10));
+
+console.log(buttons[0])
